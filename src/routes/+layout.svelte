e6febedcf529ba4a894234b8375cbffe070fdb73
edde746 <86283021+edde746@users.noreply.github.com>
2023-10-31 08:40:35 +0100

    bugfixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
   import "../app.postcss";
 </script>
 
-<slot />
+<div class="max-w-5xl mx-auto px-4">
+  <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
   import Spinner from "$lib/Spinner.svelte";
   import Masonry from "$lib/Masonry.svelte";
   import { onMount } from "svelte";
-  import { createTagsInput, melt } from "@melt-ui/svelte";
+  import { createTagsInput, melt, type Tag } from "@melt-ui/svelte";
   import Post from "./Post.svelte";
   import { goto } from "$app/navigation";
   import { browser } from "$app/environment";
@@ -15,14 +15,30 @@
     states: { tags },
   } = createTagsInput({
     unique: true,
-    defaultTags: $page.url.searchParams.get("tags")?.split(" ") ?? [],
+    defaultTags: $page.url.searchParams.get("tags")?.length
+      ? $page.url.searchParams.get("tags")!.split(" ")
+      : [],
+    onTagsChange: ({ curr, next }) => {
+      postsInView = [];
+      posts = [];
+      pageIndex = 1;
+      outOfPosts = false;
+
+      const targetTags = next.map((t) => ({
+        ...t,
+        value: t.value.toLowerCase().replace(/ /g, "_"),
+      }));
+
+      fetchNextPage(targetTags);
+      return targetTags;
+    },
   });
 
   let posts: Post[] = [];
   let postsInView: string[] = [];
   let errored: number[] = [];
 
-  let pageIndex = parseInt($page.url.searchParams.get("page") || "1");
+  let pageIndex = parseInt($page.url.searchParams.get("page") || "0");
 
   let loading = false,
     outOfPosts = false;
@@ -31,12 +47,12 @@
     return errored.includes(post.id) ? count : count + 1;
   }, 0);
 
-  const fetchNextPage = () => {
+  const fetchNextPage = (tagOverwrite?: Tag[]) => {
     if (loading || outOfPosts) return;
     loading = true;
 
     const params: Record<string, string> = {
-      query: $tags.map((t) => t.value).join(" "),
+      query: (tagOverwrite ?? $tags).map((t) => t.value).join(" "),
       page: (pageIndex++).toString(),
     };
 
@@ -49,23 +65,20 @@
       });
   };
 
-  onMount(() => {
-    fetchNextPage();
-    tags.subscribe(() => {
-      postsInView = [];
-      posts = [];
-      pageIndex = 1;
-      outOfPosts = false;
-      fetchNextPage();
-    });
-  });
+  onMount(fetchNextPage);
 
   $: browser &&
-    goto(`?tags=${$tags.map((t) => t.value).join(" ")}&page=${pageIndex - 1}`, {
-      replaceState: true,
-      noScroll: true,
-      invalidateAll: false,
-    });
+    goto(
+      `?${new URLSearchParams({
+        tags: $tags.length ? $tags.map((t) => t.value).join(" ") : "",
+        page: (pageIndex - 1).toString(),
+      })}`,
+      {
+        replaceState: true,
+        noScroll: true,
+        invalidateAll: false,
+      }
+    );
 </script>
 
 <svelte:window
@@ -84,46 +97,44 @@
   />
 </svelte:head>
 
-<div class="max-w-5xl mx-auto px-4">
-  <div class="py-4 solid-bg sticky top-0 z-50">
-    <div
-      use:melt={$root}
-      class="w-full z-50 focus:outline-none dark:bg-neutral-900 bg-neutral-200 px-4 py-2 rounded-lg flex gap-2"
-    >
-      {#each $tags as t}
-        <div
-          use:melt={$tag(t)}
-          class="flex items-center bg-neutral-800 rounded-md pl-1.5"
+<div class="py-4 solid-bg sticky top-0 z-50">
+  <div
+    use:melt={$root}
+    class="w-full z-50 focus:outline-none dark:bg-neutral-900 bg-neutral-200 px-4 py-2 rounded-lg flex gap-2 overflow-x-auto"
+  >
+    {#each $tags as t}
+      <div
+        use:melt={$tag(t)}
+        class="flex items-center bg-neutral-800 rounded-md pl-1.5"
+      >
+        <span>
+          {t.value}
+        </span>
+        <button
+          use:melt={$deleteTrigger(t)}
+          class="flex h-full items-center rounded-md px-1.5 hover:opacity-90"
         >
-          <span>
-            {t.value}
-          </span>
-          <button
-            use:melt={$deleteTrigger(t)}
-            class="flex h-full items-center rounded-md px-1.5 hover:opacity-90"
-          >
-            <Icon src={X} class="w-3 h-3" />
-          </button>
-        </div>
-        <div
-          use:melt={$edit(t)}
-          class="flex items-center overflow-hidden rounded-md px-1.5 [word-break:break-word] data-[invalid-edit]:focus:!ring-red-500"
-        />
-      {/each}
-      <input
-        use:melt={$input}
-        type="text"
-        placeholder="Enter tags..."
-        class="data-[invalid]:text-red-500 bg-transparent"
+          <Icon src={X} class="w-3 h-3" />
+        </button>
+      </div>
+      <div
+        use:melt={$edit(t)}
+        class="flex items-center overflow-hidden rounded-md px-1.5 [word-break:break-word] data-[invalid-edit]:focus:!ring-red-500"
       />
-    </div>
+    {/each}
+    <input
+      use:melt={$input}
+      type="text"
+      placeholder="Enter tags..."
+      class="data-[invalid]:text-red-500 bg-transparent"
+    />
   </div>
-  <Masonry items={posts} let:item={post} let:index={i}>
-    {#if post}
-      <Post on:error={() => (errored = [...errored, post.id])} {post} />
-    {/if}
-  </Masonry>
 </div>
+<Masonry items={posts} let:item={post} let:index={i}>
+  {#if post}
+    <Post on:error={() => (errored = [...errored, post.id])} {post} />
+  {/if}
+</Masonry>
 
 {#if loading}
   <div class="flex w-full justify-center my-5">
