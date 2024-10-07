6bc403673f3d74add3066aef5f2456e1c8c2640e
edde746 <86283021+edde746@users.noreply.github.com>
2024-10-07 06:25:41 +0200

    update domain

--- src/routes/[...tags]/+server.ts
+++ src/routes/[...tags]/+server.ts
@@ -1,5 +1,5 @@
 import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ url }) => {
-  redirect(301, `https://hntai.lol/` + url.toString().split('/').slice(3).join('/'));
+  redirect(301, `https://rule34.pw/` + url.toString().split('/').slice(3).join('/'));
 };
